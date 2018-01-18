bd878b62c74b10ce3c5848722e929f4baf99567d
manbc <mbchen1104@gmail.comgit config --global user.email mbchen1104@gmail.comgit config --global user.email mbchen1104@gmail.com>
2018-01-18 00:19:40 -0800

    attempt to fix indentation on github

--- primes.rb
+++ primes.rb
@@ -1,21 +1,23 @@
 #finds all prime numbers up to n and returns it as a array
+
 def sieve(n)
-	number_array = Array.new()
-	(2..n).each do |x|
-		number_array[x] = x
-	end
-	#only loops up to sqrt(n) truncated so x * x < n
-	(2..Math.sqrt(n).to_i).each do |x|
-		if (number_array[x] != nil) 
-			y = x * x
-			while y <= n do
-				number_array[y] = nil
-				y += x
-			end
-		end
-	end
-	number_array.select! { |x| x != nil }
-	puts number_array
+  number_array = Array.new()
+  (2..n).each do |x|
+    number_array[x] = x
+  end
+
+  #only loops up to sqrt(n) truncated so x * x < n
+  (2..Math.sqrt(n).to_i).each do |x|
+    if (number_array[x] != nil) 
+      y = x * x
+      while y <= n do
+        number_array[y] = nil
+        y += x
+      end
+    end
+  end
+  number_array.select! { |x| x != nil }
+  puts number_array
 end
 
 sieve(1000)
