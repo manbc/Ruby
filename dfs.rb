02e4d34d800ea25833482352c54da6f2cb5ae147
manbc <mbchen1104@gmail.comgit config --global user.email mbchen1104@gmail.comgit config --global user.email mbchen1104@gmail.com>
2018-01-29 17:50:21 -0800

    finished test two

--- dfs.rb
+++ dfs.rb
@@ -1,8 +1,8 @@
 #depth first search implementation
-$count = 0
+$order = 0
 def depth_first_search(matrix, vertices_array, vertex)
-  $count += 1
-  vertices_array[vertex] = $count
+  $order += 1
+  vertices_array[vertex] = $order
   for i in 0...matrix.length
     # if there is an edge and the vertex is not visited
     if (matrix[vertex][i] == 1 && vertices_array[i] == 0)
@@ -11,6 +11,12 @@ def depth_first_search(matrix, vertices_array, vertex)
   end
 end
 
+#usage: can only be used after depth_first_search has been used
+def display_order(vertices_hash)
+  vertices_hash.each { |key, value|
+    puts "Vertex #{key} was visited: #{value}"
+  }
+end
           #vertices: a  b  c  d  e  f
 ADJACENCY_MATRIX = [[0, 0, 1, 1, 1, 0],
                     [0, 0, 0, 0, 1, 1],
@@ -19,9 +25,8 @@ ADJACENCY_MATRIX = [[0, 0, 1, 1, 1, 0],
                     [1, 1, 0, 0, 0, 1],
                     [0, 1, 1, 0, 1, 0]]
 
-NUM_OF_VERTICES = 6
 vertices = Hash.new(0)
-for i in 0...NUM_OF_VERTICES
+for i in 0...6
   vertices[i] = 0
 end
 vertices.each { |key, value|
@@ -30,18 +35,22 @@ vertices.each { |key, value|
   end
 }
 
-vertices.each { |key, value|
-  puts "Vertex #{key} was visited: #{value}"
-}
+display_order(vertices)
                #a b c d
 test_matrix = [[0,1,0,0],
                [1,0,1,1],
                [0,1,0,0],
-               [0,1,,0,]]
-
+               [0,1,0,0]]
+$order = 0
 vertices2 = Hash.new(4)
 for i in 0...4
-  #vertices
+  vertices2[i] = 0
 end
+vertices2.each { |key, value|
+  if value == 0
+    depth_first_search(test_matrix, vertices2, key)
+  end
+}
 
-
+puts "Test Matrix"
+display_order(vertices2)
